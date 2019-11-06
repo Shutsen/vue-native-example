d7931bd08ec692b00d46fe58f07745d8a4ee3dfc
Shutsen <vancampenhoutgeert@gmail.com>
2019-11-06 10:23:04 +0100

    Initial commit

---
+++ App.vue
@@ -0,0 +1,32 @@
+<template>
+  <view class="container">
+    <button :title="message" :on-press="handleBtnPress"/>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello World!"
+    }
+  },
+  methods: {
+    handleBtnPress: function() {
+      alert('Button pressed');
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
